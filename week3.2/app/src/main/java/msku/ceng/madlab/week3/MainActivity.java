5a9843098dbbc52c34257e46314764a7aa923a80
Ulas Akkoyun <ulasakkoyun26@gmail.com>
2025-10-10 15:50:05 +0300

    week3.2

--- week3.2/app/src/main/java/msku/ceng/madlab/week3/MainActivity.java
+++ week3.2/app/src/main/java/msku/ceng/madlab/week3/MainActivity.java
@@ -1,5 +1,6 @@
 package msku.ceng.madlab.week3;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -36,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listview);
         PostAdapter postAdapter = new PostAdapter(this, postList);
         listView.setAdapter(postAdapter);
+
         btnPost = findViewById(R.id.btnPost);
         btnPost.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,5 +47,19 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, POST_REQUEST);
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == POST_REQUEST && resultCode == Activity.RESULT_OK){
+            Post post = new Post();
+            post.setMessage(data.getCharSequenceExtra("msg").toString());
+            post.setImage(data.getParcelableExtra("bitmap"));
+            postList.add(post);
+
+            ((PostAdapter)listView.getAdapter()).notifyDataSetChanged();
+        }
     }
 }
--- week3.2/app/src/main/java/msku/ceng/madlab/week3/PostActivity.java
+++ week3.2/app/src/main/java/msku/ceng/madlab/week3/PostActivity.java
@@ -1,14 +1,19 @@
 package msku.ceng.madlab.week3;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -45,25 +50,44 @@ public class PostActivity extends AppCompatActivity {
                 startActivityForResult(intent, CAPTURE_IMAGE);
             }
         });
-        btnOK = findViewById(R.id.btnOK);
-        btnOK.setOnClickListener(new View.OnClickListener()){
 
-        }
+        btnOK = findViewById(R.id.btnOk);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                Bundle bundle = new Bundle();
+                bundle.putCharSequence("msg", txtMessage.getText());
+                bundle.putParcelable("bitmap", ((BitmapDrawable)img.getDrawable()).getBitmap());
+                intent.putExtras(bundle);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(Activity.RESULT_CANCELED);
+                finish();
+            }
+        });
+
 
 
 
 
         btnCancel = findViewById(R.id.btnCancel);
-
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == CAPTURE_IMAGE && resultCode == PostActivity.RESULT_OK){
             Bundle bundle = data.getExtras();
             Bitmap image = (Bitmap) bundle.get("data");
-
+            img.setImageBitmap(image);
         }
     }
 }
